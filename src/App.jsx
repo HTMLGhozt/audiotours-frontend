e8bccfe0e464ac53ada2032744d1398ea28f96e9
Thomas Dillard <thomas.dillard.studios@gmail.com>
2018-01-18 23:00:15 -0700

    connecting success!

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import React, { Component, Fragment } from 'react';
 import { Route, Link } from 'react-router-dom';
 import Home from './components/home.jsx';
 import Creator from './components/creator.jsx';
+import './loginForm.css';
 
 const Navigation = props => (
   <nav>
@@ -18,17 +19,16 @@ class App extends Component {
       user: {},
     };
   }
-  /* {
-	"username": "tdillard123",
-	"password": "T3st!234"
-} */
-  componentDidMount() {
-    fetch('http://localhost:3000/api/users/login', {
-      method: 'POST',
-      body: JSON.stringify({
+  /* 
         'username': "tdillard123",
         'password': "T3st!234",
-      }),
+  */
+
+  login(username, password) {
+    // console.log(JSON.stringify({ username, password }));
+    fetch('http://localhost:3000/api/users/login', {
+      method: 'POST',
+      body: JSON.stringify({username, password}),
       headers: {
         'Access-Control-Allow-Origin': '*',
         'Content-Type': 'application/json',
@@ -36,19 +36,39 @@ class App extends Component {
       mode: 'cors'
     })
       .then(async res => {
-        const user = await res.json();
-        await this.setState({ user });
-      })
+        console.log(res);
+        if (res.status === 200) {
+          const user = await res.json();
+          await this.setState({ user });
+        }
+      });
   }
   render() {
     const { creator } = this.state.user;
-    return (
-      <Fragment>
-        <Navigation isCreator={ creator }/>
-        <Route exact path={'/'} component={Home} />
-        <Route path={'/creator'} component={Creator} />
-      </Fragment>
-    );
+    let username, password;
+    if (Object.keys(this.state.user).length) {
+      return (
+        <Fragment>
+          <Navigation isCreator={creator} />
+          <Route exact path='/' render={() => <Home user={this.state.user} />} />
+          <Route path='/creator' render={() => <Creator user={this.state.user} />} />
+        </Fragment>
+      );
+    } else {
+      return (
+        <div className="module_background">
+          <form onSubmit={(e) => {
+            e.preventDefault();
+            this.login(username.value, password.value);
+            username.value = password.value = '';
+          }}>
+            <input type="text" ref={node => username = node} placeholder="username" />
+            <input type="text" ref={node => password = node} placeholder="password" />
+            <input type="submit" />
+          </form>
+        </div>
+      );
+    }
   }
 }
 
--- src/components/creator.jsx
+++ src/components/creator.jsx
@@ -28,23 +28,82 @@ const Table = ({ markers }) => (
 class Creator extends Component {
   constructor(props) {
     super(props);
+    this.user = props.user;
     this.state = {
-      markers: [{ key: uniqid(), lng: -91.1056957, lat: 29.9718272, title: 'test Marker', audio: null }],
+      markers: [{ key: uniqid(), lng: -91.1056957, lat: 29.9718272, title: 'test Marker', audio: 'https://example.com' }],
     };
 
     this.updateMarkers = this.updateMarkers.bind(this);
     this.addMarker = this.addMarker.bind(this);
   }
-  render = () => (
-    <Fragment>
-      <Map
-        markers={this.state.markers}
-        updateMarkers={this.updateMarkers}
-        addMarker={this.addMarker}
-      />
-      <Table markers={this.state.markers} />
-    </Fragment>
-  );
+  parseMarkers() {
+    // key: String,
+    // name: { type: String, required: true },
+    // coordinates: {
+    //   type: { type: String, default: 'Point' },
+    //   coordinates: [Number],
+    // },
+    // audio: { type: String, match: [urlRegex, 'Please fill a valid url.'] },
+    const { markers } = this.state;
+    return markers.map(({ lng, lat, title, key, audio }) => {
+      return {
+        key,
+        coordinates: { type: 'Point', coordinates: [lng, lat] },
+        name: title,
+        audio,
+      };
+    });
+  }
+  saveTour(title, description, isPublished) {
+    console.log(this.user);
+    fetch('http://localhost:3000/api/tours/newTour', {
+      method: 'POST',
+      body: JSON.stringify({
+        id: this.user.id,
+        name: title,
+        description,
+        isPublished,
+        points: this.parseMarkers(),
+      }),
+      headers: {
+        'Access-Control-Allow-Origin': '*',
+        'Content-Type': 'application/json',
+      },
+      mode: 'cors'
+    })
+      .then(async res => {
+        if (res.status === 201) {
+          const user = await res.json();
+          console.log(user);
+        }
+      });
+  }
+  render() {
+    let title, description, isPublished;
+    return (
+      <form onSubmit={(e) => {
+        e.preventDefault();
+        this.saveTour(title.value, description.value, isPublished.value);
+        title.value = description.value = isPublished.value
+      }}>
+        Title:
+        <input ref={node => title = node} type="text" />
+        Description:
+        <textarea ref={node => description = node} name="description" maxLength="280" cols="50" rows="4" />
+        <Map
+          markers={this.state.markers}
+          updateMarkers={this.updateMarkers}
+          addMarker={this.addMarker}
+        />
+        <Table markers={this.state.markers} />
+        <label htmlFor="isPublished">
+          <input ref={node => isPublished = node} value="true" type="checkbox" id="isPublished" />
+          &nbsp;Publish?
+        </label>
+        <input type="submit" value="Save" />
+      </form>
+    );
+  }
   async updateMarkers(updateMarker) {
     const markers = this.state.markers.map(marker => marker.key === updateMarker.key ? updateMarker : marker);
     console.log(markers);
